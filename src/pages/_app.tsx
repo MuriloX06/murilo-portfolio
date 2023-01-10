a460fb9ada06ae9de44f7b298399462d71c2769d
MZG0969 <muriloabreucastelobranco@gmail.com>
2023-01-10 00:31:16 -0300

    Add @vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
 
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
       <GlobalStyle />
     </ThemeProvider>
   )
