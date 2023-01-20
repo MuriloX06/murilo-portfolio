d0d9a04e2e7e588dfd75244ebc9903dfca1ff405
MZG0969 <muriloabreucastelobranco@gmail.com>
2023-01-20 01:53:28 -0300

    Added image

--- src/Components/HomeHero/index.tsx
+++ src/Components/HomeHero/index.tsx
@@ -1,9 +1,17 @@
 import { Container } from './styles'
+import { TextContainer } from './styles'
+import picture from '../../assets/pessoa.webp'
 
 export default function HomeHero() {
   return (
     <Container>
-      
+      <img src={picture} alt="Foto do Murilo" />
+      <div>
+        <TextContainer>
+          <h1>Olá</h1>
+          <h2>Me chamo Murilo</h2>
+        </TextContainer>
+      </div>
     </Container>
   )
 }
--- src/Components/HomeHero/styles.ts
+++ src/Components/HomeHero/styles.ts
@@ -1,3 +1,22 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  justify-content: center;
+  margin-top: 5rem;
+
+  > img{
+    width: 40rem;
+    flex: 1;
+  }
+
+  > div{
+    flex: 4;
+  }
+`
+
+export const TextContainer = styled.section``
+
